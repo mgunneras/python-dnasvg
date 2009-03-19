2c06fcfa999165712ef7efdb1e7ae8b5947f0610
Mattias Gunneras <mattias@localhost.localdomain>
2009-03-19 00:32:41 +0000

    Added labels for traits and first stab at trying to shift hue over different chromosomes.

--- dnasvg/dna.py
+++ dnasvg/dna.py
@@ -9,16 +9,19 @@ class SNP(object):
 	Single Nucleotide Polymorphism
 	Basically this is a row in the genome CSV
 	"""
-	def __init__(self, name, variation, strand, code):
+	def __init__(self, name, variation, chromosome, strand, code):
 		self.name = name
 		self.variation = variation
 		self.code = code
 		self.strand = strand
-	
+		self.chromosome = chromosome
+
 	def get_strand(self):
 		return (self.strand == '+')
-		
+
 	def colour(self, index):
+		if self.trait:
+			return config.COLOURS.get('TRAIT')
 		c = config.COLOURS.get(self.code[index])
 		if index == self.get_strand():
 			c = [v*1.5 for v in c]
@@ -27,14 +30,37 @@ class SNP(object):
 
 class SNPFactory(object):
 	
-	def __init__(self, file):
-		self.reader = csv.reader(file, delimiter=',')
+	def __init__(self, snp_file, trait_file):
+		self.reader = csv.reader(snp_file, delimiter=',')
 		# set over the header lables.
 		self.reader.next()
-	
+		self.chromosome = None	
+		trait_reader = csv.reader(trait_file, delimiter=',')
+		# skip labels
+		trait_reader.next()
+		self.traits = {}
+		while True:
+			try:
+				trait = trait_reader.next()
+				self.traits[trait[2]] = trait
+			except StopIteration:
+				break
+
+	def trait_for_snp(self, snp):
+		if self.chromosome != snp.chromosome:
+			print 'Chromosome', snp.chromosome
+			self.chromosome = snp.chromosome
+		return self.traits[snp.name]
+
 	def get_SNP(self):
 		data = self.reader.next()
-		return SNP(data[0], data[1], data[4], data[5])
+		snp = SNP(data[0], data[1], data[2], data[4], data[5])
+		try:
+			snp.trait = self.trait_for_snp(snp)
+			print snp.trait
+		except KeyError:
+			snp.trait = None
+		return snp
 
 
 class DNADrawer(object):
@@ -46,18 +72,27 @@ class DNADrawer(object):
 	_current_y = 0	
 	_trigon_point_bottom_left = True
 		
-	def __init__(self, deCODEme_scan_file, limit, offset, width, height):
-		self.snp_factory = SNPFactory(deCODEme_scan_file)
+	def __init__(self, deCODEme_scan_file, deCODEme_trait_file, limit, offset, width, height):
+		self.snp_factory = SNPFactory(deCODEme_scan_file, deCODEme_trait_file)
 		self.limit = limit
 		self.offset = offset
 		self.shape_size = math.sqrt((width*height)/limit)
 		self.grid_width = int(width/self.shape_size)
 		self.grid_height = int(math.floor(height/self.shape_size)) 
-		#print self.shape_size
-		#self.limit = self.grid_width*self.grid_height
-		#self.shape_size = math.sqrt((width*height)/self.limit)
-		#print self.shape_size
 
+	def get_filters(self):
+		"""
+		    <filter id="HueRotate90" filterUnits="objectBoundingBox" x="0%" y="0%" width="100%" height="100%">
+			     <feColorMatrix type="hueRotate" in="SourceGraphic" values="190"/>
+			</filter>
+		"""
+		defs = SVG('defs')
+		for c in range(22):
+			c=c+1
+			filter = SVG('filter', id='F%d' % c, filterUnits="objectBoundingBox", x="0%", y="0%", width="100%", height="100%")
+			filter.append(SVG('feColorMatrix', type="hueRotate", values="%d0" % (int(c)*3)))
+			defs.append(filter)
+		return defs
 
 	def get_svg(self):
 		return self._render()
@@ -66,8 +101,10 @@ class DNADrawer(object):
 		"""
 		renders the SVG representation and wraps it in a group.
 		"""
-		self.svg_group = SVG('g')
+		#self.svg_group = SVG('g')
 		shape_count = 0
+		base_group = SVG('g', id='base_group')
+		chromosome = None
 		while True:
 			try:
 				while True:
@@ -79,13 +116,18 @@ class DNADrawer(object):
 			if shape_count < self.offset: 
 				shape_count=shape_count+1
 				continue
+			# create a group for chromosome
+			if chromosome != snp.chromosome:
+				chromo_group = SVG('g', id=snp.chromosome, filter="url(#F%s)" % snp.chromosome)
+				base_group.append(chromo_group)
+				chromosome=snp.chromosome
 			svg = self.svg_shape(snp)
-			self.svg_group.append(svg)
+			chromo_group.append(svg)
 			self._calculate_new_pos()
 			shape_count=shape_count+1
 			if shape_count==self.limit: break
 			if self._current_y==self.grid_height: break
-		return self.svg_group
+		return base_group
 
 	def svg_shape(self, snp):
 		"""
@@ -100,7 +142,8 @@ class DNADrawer(object):
 			points_string = "%d,%d %d,%d %d,%d" % (p[0][0], p[0][1], p[1][0], p[1][1], p[2][0], p[2][1])
 			colour = snp.colour(index)
 			group.append(SVG("polygon", points=points_string, fill=rgb(colour[0], colour[1], colour[2], 255)))
-
+		if snp.trait:
+			group.append(SVG("text", SVG("tspan", snp.trait[0]), x=self.x(), y=self.y()-2, fill="black", font_size="10pt"))
 		return group
 	
 	def trigon_points(self, point_left):
--- dnasvg/draw.py
+++ dnasvg/draw.py
@@ -7,37 +7,40 @@ import dna
 import svgfig
 
 def main():
-	usage = "usage: %prog [options] inputfile outputfile"
+	usage = "usage: %prog [options] inputfile outputfile traitfile"
 	parser = OptionParser(usage=usage)
 	parser.add_option("-l", "--limit", 		default=config.LIMIT, help="Max limit of SNPs to draw")
 	parser.add_option("-o", "--offset", 	default=config.OFFSET, help="Offset of SNPs to skip")
 	parser.add_option("-v", "--viewport",	default=config.CANVAS_VIEWPORT, help="Viewport to draw", metavar="WIDTHxHEIGHT")
+	parser.add_option("-m", "--multiply",	default=1, help="Multiply the viewport sizes", metavar="FACTOR")
 	parser.add_option("-s", "--stroke",		default=config.STROKE, help="Stroke width", metavar="X.Xpt")
 
 	(options, args) = parser.parse_args()
 
-	if len(args) != 2:
+	if len(args) != 3:
 		parser.error("incorrect number of arguments")
 	
 	# options
 	limit 		= int(options.limit)
 	offset 		= int(options.offset)
 	viewport 	= options.viewport
+	multiply 	= int(options.multiply)
 	stroke 		= options.stroke
 	
-	if type(viewport) == str:
-		viewport = [int(d) for d in viewport.split('x')]
+	viewport = [int(d)*multiply for d in viewport.split('x')]
 
 	input_file = open(args[0], "r")
-	dna_drawer = dna.DNADrawer(input_file, limit, offset, viewport[0], viewport[1])
+	trait_file = open(args[2], "r")
+	dna_drawer = dna.DNADrawer(input_file, trait_file, limit, offset, viewport[0], viewport[1])
+	svg_filters = dna_drawer.get_filters()
 	dnasvg = dna_drawer.get_svg()
 	input_file.close()
+	trait_file.close()
 
 	canvas_width = "%dpx" % viewport[0]
 	canvas_height = "%dpx" % viewport[1]
 	canvas_viewbox = "0 0 %d %d" % (viewport[0], viewport[1])
-	
-	svgcanvas = svgfig.canvas(dnasvg, width=canvas_width, height=canvas_height, viewBox=canvas_viewbox, style="stroke:white; fill:cornsilk; stroke-width:%s; stroke-linejoin:round; text-anchor:middle" % stroke)
+	svgcanvas = svgfig.canvas(svg_filters, dnasvg, width=canvas_width, height=canvas_height, viewBox=canvas_viewbox, style="stroke:white; fill:cornsilk; stroke-width:%s; stroke-linejoin:round; text-anchor:middle" % stroke)
 	svgcanvas.save(args[1])
 
 
